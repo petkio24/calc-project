ce56138aae00768d4c95203fde5bd99b2e396415
petkio24 <petkio24@example.com>
2025-10-29 00:34:12 +0300

    проект с меню и аунтификацией

--- routes/web.php
+++ routes/web.php
@@ -1,22 +1,18 @@
 <?php
 
+use App\Http\Controllers\HomeController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/login');
 });
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/home', [HomeController::class, 'index'])->name('home');
+
+// Защищаем все остальные маршруты аутентификацией
+Route::middleware(['auth'])->group(function () {
+    // Здесь будут все защищенные маршруты
+});
